f840bf3d3ea57a25720c95a52406a38ba14bcdc9
Shoheicode <40525329+Shoheicode@users.noreply.github.com>
2024-05-16 16:35:18 -0700

    Updated Main File

--- personalwebsite/lib/main.dart
+++ personalwebsite/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personalwebpage/Testpage.dart';
 import 'package:personalwebpage/landingpage.dart';
 
 void main() {
@@ -11,9 +12,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: LandingPage(),
+      home: TestPage(),
       debugShowCheckedModeBanner: false,
-      
+
       );
   }
 }
