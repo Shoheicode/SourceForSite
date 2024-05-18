e00b9073895f3139e889756c74bf5d2858365f8b
Shoheicode <40525329+Shoheicode@users.noreply.github.com>
2024-05-18 14:24:57 -0700

    Added the card

--- personalwebsite/lib/card.dart
+++ personalwebsite/lib/card.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
 
-class Card extends StatefulWidget{
+class ImageCard extends StatefulWidget{
 
-  Card(this.text, this.image, {super.key});
+  ImageCard(this.text, this.image, {super.key});
 
   String text;
   String image;
+
+  
+
   @override
-  State<StatefulWidget> createState() => CardState();
+  State<StatefulWidget> createState() => CardState(text, image);
 
 }
 
-class CardState extends State<Card>{
+class CardState extends State<ImageCard>{
+  
+  late String text;
+  late String image;
+
+  CardState(String a, String b){
+    text = a;
+    image = b;
+  }
+
   @override
   Widget build(BuildContext context) {
+    String a = "hi";
     // TODO: implement build
-    return Scaffold(
-      body: Text("hdhalfjdlk"),
+    return Container(
+        decoration: const BoxDecoration(          
+          color: Colors.white
+        ),
+          child: Column(
+          children: <Widget>[
+            Image.asset("assets/profile.png", height: 300),
+            Text(text)
+          ]
+        ),
     );
   }
   
