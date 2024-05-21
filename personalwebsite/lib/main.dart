6818f162f4dac7fa53175e9c35a838e3ed428a87
Shoheicode <40525329+Shoheicode@users.noreply.github.com>
2024-05-21 11:31:31 -0700

    updated main dart

--- personalwebsite/lib/main.dart
+++ personalwebsite/lib/main.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:personalwebpage/Testpage.dart';
 import 'package:personalwebpage/landingpage.dart';
 
+import 'package:firebase_ui_auth/firebase_ui_auth.dart'; // new
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';               // new
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';                 // new
+
+import 'ApplicationState/app_state.dart';
+
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(ChangeNotifierProvider(
+    create: (context)=>ApplicationState(),
+    builder: ((context, child) =>const MainApp()),
+  ));
 }
 
 class MainApp extends StatelessWidget {
